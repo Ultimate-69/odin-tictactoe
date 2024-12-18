6cb09e1314cd9f1ad9242b2a3d83859ba7114e6c
Ultimate-69 <170136337+Ultimate-69@users.noreply.github.com.>
2024-12-18 16:02:44 +0200

    implemented better ui to display score, and change the player names.

--- main.js
+++ main.js
@@ -1,12 +1,52 @@
 const table = document.querySelector('.table');
 const restart = document.querySelector('.restart');
+const score = document.querySelector('.score');
+const scoreButton = document.querySelector('.show-score');
+
+const player1Input = document.querySelector('.player1name');
+const player2Input = document.querySelector('.player2name');
+
+let player1Name = "Player 1";
+let player2Name = "Player 2";
+
+let player1Score = 0;
+let player2Score = 0;
 
 const board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 let turn = 1;
 let canPlay = true;
 
+scoreButton.addEventListener('click', () => {
+    displayScore();
+})
+
 restart.addEventListener('click', () => {
-    location.reload();
+    board.forEach((value, index) => {
+        board[index] = 0;
+    })
+    canPlay = true;
+    turn = 1;
+    const result = document.querySelector('.result');
+    result.innerHTML = "";
+    renderTableHtml();
+})
+
+player1Input.addEventListener('input', () => {
+    if (player1Input.value.trim() !== "") {
+        player1Name = player1Input.value;
+    }
+    else {
+        player1Name = "Player 1";
+    }
+})
+
+player2Input.addEventListener('input', () => {
+    if (player2Input.value.trim() !== "") {
+        player2Name = player2Input.value;
+    }
+    else {
+        player2Name = "Player 2";
+    }
 })
 
 renderTableHtml();
@@ -65,21 +105,25 @@ function playTurn(position) {
             turn = 1;
         }
     
-        console.log(board);
         const win = checkWin();
         if (win === 1) {
             const result = document.querySelector('.result');
-            result.innerHTML = "Player 1 wins!"
+            result.innerHTML = `${player1Name} wins!`
+            player1Score++;
+            displayScore();
             canPlay = false;
         }
         else if (win === 2) {
             const result = document.querySelector('.result');
-            result.innerHTML = "Player 2 wins!"
+            result.innerHTML = `${player2Name} wins!`
+            player2Score++;
+            displayScore();
             canPlay = false;
         }
         else if (win === 3) {
             const result = document.querySelector('.result');
             result.innerHTML = "Tie!"
+            displayScore();
             canPlay = false;
         }
     }
@@ -161,4 +205,21 @@ function checkWin() {
             return 3;
         }
     })
+}
+
+function displayScore() {
+    score.showModal();
+    score.innerHTML = 
+    `
+    <div>
+        ${player1Name}: ${player1Score}
+        <br>
+        ${player2Name}: ${player2Score}
+        <button onclick="closeScore()">Close</button>
+    </div>
+    `;
+}
+
+function closeScore() {
+    score.close();
 }
